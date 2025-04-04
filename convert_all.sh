9916f8bb085398b4b97e5bcd7c1706c1a7f897e6
Alain Vagner <alain@sous-anneau.org>
2025-04-04 10:59:30 +0200

    bug fix in the batch processing

--- convert_all.sh
+++ convert_all.sh
@@ -7,5 +7,5 @@ elif [[ "$OSTYPE" == "win32" ]]; then
         node_bin="node.exe"
 fi
 
-mkdir -p "$1/out/"
-find "$1" -type f | while read i; do echo $i; $node_bin audit_convert.js $i > "$1/json/"`basename $i`.json; done
+mkdir -p "$1/json/"
+find "$1/" -type f -name "*.xlsx"  | while read i; do echo $i; $node_bin audit_convert.js $i > "$1/json/"`basename $i`.json; done
